c220341ad146e7499563a03b63da3e928b03538c
huykhoa11 <69473375+huykhoa11@users.noreply.github.com>
2021-10-14 18:28:53 +0900

    first commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.cmbDrawKind = new System.Windows.Forms.ComboBox();
+            this.cmbPenStyle = new System.Windows.Forms.ComboBox();
+            this.txtPenWidth = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionInfomationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label1.Location = new System.Drawing.Point(43, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shape";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label2.Location = new System.Drawing.Point(191, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Line";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label3.Location = new System.Drawing.Point(343, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Width of Line";
+            // 
+            // btnColor
+            // 
+            this.btnColor.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnColor.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.btnColor.Location = new System.Drawing.Point(513, 41);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(79, 36);
+            this.btnColor.TabIndex = 1;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // cmbDrawKind
+            // 
+            this.cmbDrawKind.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmbDrawKind.FormattingEnabled = true;
+            this.cmbDrawKind.Items.AddRange(new object[] {
+            "Line",
+            "Rectangle",
+            "Circle",
+            "Ellipse",
+            "Free"});
+            this.cmbDrawKind.Location = new System.Drawing.Point(47, 58);
+            this.cmbDrawKind.Name = "cmbDrawKind";
+            this.cmbDrawKind.Size = new System.Drawing.Size(96, 20);
+            this.cmbDrawKind.TabIndex = 2;
+            this.cmbDrawKind.SelectedIndexChanged += new System.EventHandler(this.cmbDrawKind_SelectedIndexChanged);
+            // 
+            // cmbPenStyle
+            // 
+            this.cmbPenStyle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cmbPenStyle.FormattingEnabled = true;
+            this.cmbPenStyle.Items.AddRange(new object[] {
+            "Solid",
+            "Dash",
+            "Dot",
+            "DashDot",
+            "DashDotDot",
+            "Fill"});
+            this.cmbPenStyle.Location = new System.Drawing.Point(195, 58);
+            this.cmbPenStyle.Name = "cmbPenStyle";
+            this.cmbPenStyle.Size = new System.Drawing.Size(96, 20);
+            this.cmbPenStyle.TabIndex = 2;
+            this.cmbPenStyle.SelectedIndexChanged += new System.EventHandler(this.cmbPenStyle_SelectedIndexChanged);
+            // 
+            // txtPenWidth
+            // 
+            this.txtPenWidth.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtPenWidth.Location = new System.Drawing.Point(347, 58);
+            this.txtPenWidth.Name = "txtPenWidth";
+            this.txtPenWidth.Size = new System.Drawing.Size(100, 19);
+            this.txtPenWidth.TabIndex = 3;
+            this.txtPenWidth.TextChanged += new System.EventHandler(this.txtPenWidth_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(782, 339);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "bmpファイル|*.bmp|すべてのファイル|*.*";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.versionInfomationsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // versionInfomationsToolStripMenuItem
+            // 
+            this.versionInfomationsToolStripMenuItem.Name = "versionInfomationsToolStripMenuItem";
+            this.versionInfomationsToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.versionInfomationsToolStripMenuItem.Text = "Version and Infomations";
+            this.versionInfomationsToolStripMenuItem.Click += new System.EventHandler(this.versionInfomationsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtPenWidth);
+            this.Controls.Add(this.cmbPenStyle);
+            this.Controls.Add(this.cmbDrawKind);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Paint Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.ComboBox cmbDrawKind;
+        private System.Windows.Forms.ComboBox cmbPenStyle;
+        private System.Windows.Forms.TextBox txtPenWidth;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionInfomationsToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        enum DrawKind {LINE, RECTANGLE, CIRCLE, ELLIPSE, FREE }; //図形の種類
+
+        bool flagMouseDown = false ;
+        bool btnColorClick = false;
+        int x1, y1, x2, y2;
+        int penWidth = 5;
+
+        DrawKind drawkind;
+        ColorDialog colorDlg = new ColorDialog();
+        Pen pen;
+        SolidBrush brush;
+        Bitmap bmp1;
+        Graphics g;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbDrawKind.Text = "Line";
+            cmbPenStyle.Text = "Solid";
+            txtPenWidth.Text = "1";
+            Console.WriteLine("Hello");
+            pen = new Pen(Color.Black, penWidth);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+
+        private void cmbPenStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbPenStyle.Text == "Solid")
+            {
+                if(btnColorClick == false)  pen = new Pen(Color.Black, penWidth);
+                else pen = new Pen(colorDlg.Color, penWidth);
+            }
+
+            else if (cmbPenStyle.Text == "Dash")
+            {
+                pen.DashStyle= System.Drawing.Drawing2D.DashStyle.Dash;
+            }
+
+            else if (cmbPenStyle.Text == "Dot")
+            {
+                pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            }
+
+            else if (cmbPenStyle.Text == "DashDot")
+            {
+                pen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+            }
+
+            else if (cmbPenStyle.Text == "DashDotDot")
+            {
+                pen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+            }
+
+            else if (cmbPenStyle.Text == "Fill")
+            {
+                brush = new SolidBrush(colorDlg.Color);
+            }
+        }
+
+        private void cmbDrawKind_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbDrawKind.Text == "Line")
+            {
+                drawkind = DrawKind.LINE;
+            }
+            else if (cmbDrawKind.Text == "Rectangle")
+            {
+                drawkind = DrawKind.RECTANGLE;
+            }
+            else if (cmbDrawKind.Text == "Circle")
+            {
+                drawkind = DrawKind.CIRCLE;
+            }
+            else if (cmbDrawKind.Text == "Ellipse")
+            {
+                drawkind = DrawKind.ELLIPSE;
+            }
+            else if(cmbDrawKind.Text == "Free")
+            {
+                drawkind = DrawKind.FREE;
+            }
+        }
+
+
+        private void txtPenWidth_TextChanged(object sender, EventArgs e)
+        {
+            penWidth = Convert.ToInt32(txtPenWidth.Text);
+            pen = new Pen(colorDlg.Color, penWidth);
+        }
+
+        private void btnColor_Click(object sender, System.EventArgs e)
+        {
+            btnColorClick = true;
+            colorDlg.ShowDialog();
+            btnColor.BackColor = colorDlg.Color;
+            pen = new Pen(colorDlg.Color, penWidth);
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x1 = e.X;
+            y1 = e.Y;
+            if (drawkind != DrawKind.FREE) pictureBox1.Cursor = Cursors.Cross;
+
+            flagMouseDown = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {   
+
+            if (flagMouseDown == true)
+            {
+                x2 = e.X;
+                y2 = e.Y;
+                pictureBox1.Refresh();
+                bmp1 = new Bitmap(pictureBox1.Image);
+                g = Graphics.FromImage(bmp1);
+                if (drawkind == DrawKind.FREE)
+                {
+                    g.DrawLine(pen, new Point(x1, y1), e.Location);
+                    pictureBox1.Image = bmp1;
+                    x1 = x2;
+                    y1 = y2;
+                }
+            }
+        }
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            bmp1 = new Bitmap(pictureBox1.Image);
+            g = Graphics.FromImage(bmp1);
+            int xx1, xx2, yy1, yy2, xc, yc, rr, w, h;
+            if (drawkind == DrawKind.LINE)
+            {
+                if (cmbPenStyle.Text == "Fill") g.DrawLine(new Pen(colorDlg.Color, penWidth), x1, y1, x2, y2);
+                else g.DrawLine(pen, x1, y1, x2, y2);
+            }
+
+            else if (drawkind == DrawKind.RECTANGLE)
+            {
+                w = Math.Abs(x2 - x1);
+                h = Math.Abs(y2 - y1);
+                if (cmbPenStyle.Text == "Fill") g.FillRectangle(new SolidBrush(colorDlg.Color), x1, y1, w, h);
+                else
+                {
+                    g.DrawRectangle(pen, x1, y1, w, h);
+                    pictureBox1.Image = bmp1;
+                }
+            }
+
+            else if (drawkind == DrawKind.CIRCLE)
+            {
+                rr = (int)(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) / 2.0);
+                xc = (x1 + x2) / 2;
+                yc = (y1 + y2) / 2;
+                xx1 = xc - rr;
+                xx2 = xc + rr;
+                yy1 = yc - rr;
+                yy2 = yc + rr;
+                w = xx2 - xx1;
+                h = yy2 - yy1;
+                if (cmbPenStyle.Text == "Fill") g.FillEllipse(new SolidBrush(colorDlg.Color), xx1, yy1, w, h);
+                else
+                {
+                    g.DrawEllipse(pen, xx1, yy1, w, h);
+                    pictureBox1.Image = bmp1;
+                }       
+            }
+
+            else if (drawkind == DrawKind.ELLIPSE)
+            {
+                w = x2 - x1;
+                h = y2 - y1;
+                if (cmbPenStyle.Text == "Fill") g.FillEllipse(new SolidBrush(colorDlg.Color), x1, y1, w, h);
+                else
+                {
+                    g.DrawEllipse(pen, x1, y1, w, h);
+                    pictureBox1.Image = bmp1;
+                }
+            }
+
+            else if (drawkind == DrawKind.FREE)
+            {
+
+                Console.WriteLine("dmm");
+                g.DrawLine(pen, new Point(x1, y1), e.Location);
+                pictureBox1.Image = bmp1;
+                x1 = x2;
+                y1 = y2;
+            }
+            g.Dispose();       
+            pictureBox1.Image = bmp1;
+            flagMouseDown = false;
+            pictureBox1.Cursor = Cursors.Default;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (flagMouseDown == true)
+            {
+                int xx1, xx2, yy1, yy2, xc, yc, rr, w, h;
+                if (drawkind == DrawKind.LINE)
+                {
+                    if (cmbPenStyle.Text == "Fill") e.Graphics.DrawLine(new Pen(colorDlg.Color, penWidth), x1, y1, x2, y2); 
+                    else e.Graphics.DrawLine(pen, x1, y1, x2, y2);
+                }
+
+                else if (drawkind == DrawKind.RECTANGLE)
+                {
+                    w = Math.Abs(x2 - x1);
+                    h = Math.Abs(y2 - y1);
+                    if (cmbPenStyle.Text == "Fill") e.Graphics.FillRectangle(new SolidBrush(colorDlg.Color), x1, y1, w, h);
+                    else e.Graphics.DrawRectangle(pen, x1, y1, w, h);
+                }
+
+                else if (drawkind == DrawKind.CIRCLE)
+                {
+                    rr = (int)(Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) / 2.0);
+                    xc = (x1 + x2) / 2;
+                    yc = (y1 + y2) / 2;
+                    xx1 = xc - rr;
+                    xx2 = xc + rr;
+                    yy1 = yc - rr;
+                    yy2 = yc + rr;
+                    w = xx2 - xx1;
+                    h = yy2 - yy1;
+                    if (cmbPenStyle.Text == "Fill") e.Graphics.FillEllipse(new SolidBrush(colorDlg.Color), xx1, yy1, w, h);
+                    else e.Graphics.DrawEllipse(pen, xx1, yy1, w, h);
+                }
+
+                else if (drawkind == DrawKind.ELLIPSE)
+                {
+                    w = x2 - x1;
+                    h = y2 - y1;
+                    if (cmbPenStyle.Text == "Fill") e.Graphics.FillEllipse(new SolidBrush(colorDlg.Color), x1, y1, w, h);
+                    else e.Graphics.DrawEllipse(pen, x1, y1, w, h);
+                }
+            }
+        }
+
+        private void versionInfomationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by Tran Huy Khoa - 2021", "Inform",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+            colorDlg.Color = Color.Black;
+            btnColor.BackColor = default(Color);
+            btnColor.Focus();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {   
+            saveFileDialog1.ShowDialog();
+            bmp1.Save(saveFileDialog1.FileName);
+            MessageBox.Show("File saved successfully", "Inform");
+        }
+
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+                Bitmap newBmp = new Bitmap(openFileDialog1.FileName);
+
+                g = Graphics.FromImage(bmp);
+                g.DrawImage(newBmp, 0, 0, newBmp.Width, newBmp.Height);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
